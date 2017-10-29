c737fc94097e3ce0194b65443f40bd49d9561fdc
Dídac Sabatés <sabatesduran@gmail.com>
2017-10-29 23:52:47 +0100

    Print api list with info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      apis: [],
+    }
+  }
+
+  async componentWillMount() {
+    let repo = "https://raw.githubusercontent.com/toddmotto/public-apis/master/json/entries.min.json";
+    let apis = [];
+
+    await fetch(repo).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      apis = json['entries'];
+      localStorage.setItem('apis',JSON.stringify(apis));
+    });
+
+    this.setState({'apis': apis});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React - Public API's</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {
+            this.state.apis.map((api, i) => {
+              return (
+                <li key={i}>
+                  {api.API}
+                  <ul>
+                    <li>Description: {api.Description}</li>
+                    <li>Category: {api.Category}</li>
+                    <li>Link: {api.Link}</li>
+                    <li>HTTPS: {api.HTTPS ? "Yes" : "No"}</li>
+                    <li>Auth: {api.Auth ? "Yes" : "No"}</li>
+                  </ul>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     );
   }
